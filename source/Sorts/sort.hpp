c03b3b2b5bdd59f4043d4cbcbdb6dff900766337
Etebomsam <50887872+Etebomsam@users.noreply.github.com>
2019-07-19 00:00:43 -0400

    Update sort.hpp

--- source/Sorts/sort.hpp
+++ source/Sorts/sort.hpp
@@ -1,6 +1,23 @@
 #pragma once
 #include "../Functions/functions.hpp"
 
-void sort(int* array, int n);
 
-void heapsort(int* arr, int length);
+//I am goimg to copy the array of elements to LLRB Tree
+//In-Order transverse the tree and copy it back into the array 
+// average complexity of inserting n elements is O(nlogn)
+//Worst case complexity will be the same as the average case 
+// BST sort had a worse case complexity of O(n^2)
+
+void LLRBTreeSort(int array[],int n){
+    for (int i = 0; i < n; i++){
+        rbTree.insert(array[i]);
+    }
+
+    rbTree.copySortedArr(array);
+}
+
+
+
+
+//void sort(int* array, int n);
+//LLRBTreeSort(array,n);
